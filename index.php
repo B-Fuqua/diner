238da864e1e88065bf9081df0e7b312649d5d3e0
Ben Fuqua <bfuqua@mail.greenriver.edu>
2021-04-27 12:26:37 -0400

    added breakfast route

--- index.php
+++ index.php
@@ -19,5 +19,11 @@ $f3->route('GET /', function(){
     echo $view->render('views/home.html');
 });
 
+$f3->route('GET /breakfast', function(){
+    //Display the breakfast page
+    $view = new Template();
+    echo $view->render('views/breakfast.html');
+});
+
 //Run Fat-Free
 $f3->run();
